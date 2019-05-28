b5f6bf74e1aff493ec9b65abe04aed08e8f63eef
Sven den Otter <sven@labela.nl>
2019-05-28 15:43:20 +0200

    Ignore Pylint bad-continuation

--- goal_service/infrastructure/orm/schema.py
+++ goal_service/infrastructure/orm/schema.py
@@ -1,3 +1,4 @@
+# pylint: disable=bad-continuation
 from sqlalchemy import Table, Column, String, DateTime, Boolean, Integer, \
     ForeignKey
 from . import database
@@ -12,7 +13,9 @@ GOAL_TABLE = Table('goal', database.metadata,
     Column('completed', Boolean),
     Column('discarded', Boolean))
 
-GOAL_PROGRESSION_TABLE = Table('goal_progression', database.metadata,
+GOAL_PROGRESSION_TABLE = Table(
+    'goal_progression',
+    database.metadata,
     Column('id', String(36), primary_key=True),
     Column('note', String),
     Column('percentage', Integer),
